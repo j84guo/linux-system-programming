17827f8977ba88b6fb4e2528be364e481854543a
Jackson Guo <jguo@CAN-JLC-ZeroGravity16.local>
2018-08-24 12:17:20 -0400

    updated exec notes

--- 04-process-management/execv_git.c
+++ 04-process-management/execv_git.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 
 int main()
-{
+{ 
+    /*
+    * array of constant strings
+    */
     char *const args[] = {"dep", "status", NULL};
     int ret = execv("/usr/local/bin/dep", args);
     if(ret == -1) perror("git");

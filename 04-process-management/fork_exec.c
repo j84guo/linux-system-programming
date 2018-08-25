ca9a1d0312ca5ccba1bfa99fc0b41a7f23fcd8e8
Jackson Guo <jguo@CAN-JLC-ZeroGravity16.local>
2018-08-25 19:11:52 -0400

    redirect output before forking

--- 04-process-management/fork_exec.c
+++ 04-process-management/fork_exec.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 #include <sys/types.h>
+
+int redirect_outerr()
+{
+    close(1);
+    close(2);
+
+    if(open("out.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644) == -1)
+        return -1;
+
+    dup(1);
+    return 0;
+}
+
+
 
 int main()
 {
+    if(redirect_outerr() == -1)
+    {
+        perror("redirect_outerr");
+        return 1;
+    }
+
     /*
-    * e.g. spawn a child and run /bin/ls
-    */                   
+     * e.g. spawn a child and run /bin/ls
+     */                   
     pid_t pid = fork();
     
     if(pid == -1)

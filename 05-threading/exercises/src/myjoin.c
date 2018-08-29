86b0992f858096e84fc032549f8a24311ed59377
Jackson Guo <jguo@CAN-JLC-ZeroGravity16.local>
2018-08-29 08:54:30 -0400

    updated Makefile

---
+++ 05-threading/exercises/src/myjoin.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int done = 0;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t c = PTHREAD_COND_INITIALIZER;
+
+void thr_exit()
+{
+    pthread_mutex_lock(&m);
+
+    done = 1;
+    pthread_cond_signal(&c);
+
+    pthread_mutex_unlock(&m);
+}
+
+void *child(void *p)
+{
+    printf("child\n");
+    thr_exit();
+    return NULL;
+}
+
+void thr_join()
+{
+    pthread_mutex_lock(&m);
+
+    while(!done)
+        pthread_cond_wait(&c, &m);
+
+    pthread_mutex_unlock(&m);
+}
+
+int main()
+{
+    printf("parent\n");
+    pthread_t t;
+    pthread_create(&t, NULL, child, NULL);
+
+    thr_join();
+    printf("joined\n");
+    return 0;
+}

555d963ddd2fec4d1dad2b1c08cd8bc0dd42de86
Jackson Guo <guo_jackson@yahoo.ca>
2018-10-27 19:52:57 -0400

    completed intial chat client

--- 00-network-programming/examples/chat/client.c
+++ 00-network-programming/examples/chat/client.c
@@ -3,16 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
+#include <pthread.h>
 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <netdb.h>
 
-#include <pthread.h>
-#include <semaphore.h>
+#include "llist.h"
 
 typedef struct {
     int fd;
@@ -20,25 +17,26 @@ typedef struct {
 } tcpcon_t;
 
 typedef struct {
-    char ip[INET6_ADDRSTRLEN];
-    unsigned short port;
-} tcpinfo_t;
+    int pd;
+    tcpcon_t con;
+    llist_t *queue;
+} elpinfo_t;
 
-int tcpcon_config(tcpcon_t *conn, tcpinfo_t *info)
+int tcpcon_config(tcpcon_t *con, char *ip, unsigned short port)
 {
-    if (!conn || !info)
+    if (!con || !ip)
         return -1;
 
-    if (!inet_pton(AF_INET, info->ip, &conn->addr.sin_addr)) {
+    if (!inet_pton(AF_INET, ip, &con->addr.sin_addr)) {
         fprintf(stderr, "tcpcon_init: invalid ip format\n");
         return -1;
     }
 
-    conn->addr.sin_family = AF_INET;
-    conn->addr.sin_port = htons(info->port);
-    memset(&conn->addr.sin_zero, 0, sizeof conn->addr.sin_zero);
+    con->addr.sin_family = AF_INET;
+    con->addr.sin_port = htons(port);
+    memset(&con->addr.sin_zero, 0, sizeof con->addr.sin_zero);
 
-    if ((conn->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
+    if ((con->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == -1) {
         perror("socket");
         return -1;
     }
@@ -46,22 +44,22 @@ int tcpcon_config(tcpcon_t *conn, tcpinfo_t *info)
     return 0;
 }
 
-int tcpcon_destroy(tcpcon_t *conn)
+int tcpcon_destroy(tcpcon_t *con)
 {
-    if (!conn)
+    if (!con)
         return -1;
 
-    return close(conn->fd);
+    return close(con->fd);
 }
 
-int tcpcon_init(tcpcon_t *conn, tcpinfo_t *info)
+int tcpcon_init(tcpcon_t *con, char *ip, unsigned short port)
 {
-    int res = tcpcon_config(conn, info);
+    int res = tcpcon_config(con, ip, port);
     if (res != 0)
         return res;
 
-    if (connect(conn->fd, (struct sockaddr *) &conn->addr,
-        sizeof conn->addr) == -1) {
+    if (connect(con->fd, (struct sockaddr *) &con->addr,
+        sizeof con->addr) == -1) {
         perror("connect");
         return -1;
     }
@@ -87,24 +85,49 @@ int sendall(int fd, char *buf, int len)
     return 0;
 }
 
-int loop(tcpcon_t *conn)
+int loop(int pd, tcpcon_t *con, llist_t *queue)
 {
-    int n;
+    int ret;
     char rbuf[512 + 1];
+    fd_set readfds;
 
     while (1) {
-        n = recv(conn->fd, rbuf, 512, 0);
+        FD_ZERO(&readfds);
+        FD_SET(pd, &readfds);
+        FD_SET(con->fd, &readfds);
+        
+        ret = select(
+            pd >= con->fd ? pd + 1 : con->fd + 1,
+            &readfds, NULL, NULL, NULL);
+        if (ret == -1) {
+            if (errno == EINTR)
+                continue;
 
-        if (n == -1) {
-            perror("recv");
+            perror("select");
             return -1;
-        } else if (n == 0) {
-            printf("Server disconnected\n");
-            return 0;
         }
 
-        rbuf[n] = '\0';
-        printf("%s", rbuf);
+        if (FD_ISSET(pd, &readfds)) {
+            read(pd, rbuf, 1);
+
+            char *msg = llist_remf(queue);
+            sendall(con->fd, msg, strlen(msg));
+            free(msg);
+        } else {
+            ret = recv(con->fd, rbuf, 512, 0);
+
+            if (ret == -1) {
+                perror("recv");
+                return -1;
+            } else if (!ret) {
+                printf("Server disconected\n");
+                return 0;
+            }
+
+            rbuf[ret] = '\0';
+            printf("%s", rbuf);
+        }
+
     }
 
     return 0;
@@ -112,7 +135,8 @@ int loop(tcpcon_t *conn)
 
 void *run_eloop(void *arg)
 {
-    loop((tcpcon_t *) arg);
+    elpinfo_t *info = (elpinfo_t *) arg;
+    loop(info->pd, &info->con, info->queue);
     return NULL;
 }
 
@@ -122,40 +146,37 @@ void stop_thread(pthread_t tid)
     pthread_join(tid, NULL);
 }
 
-void tcpinfo_init(tcpinfo_t *info, int argc, char **argv)
-{
-    strncpy(info->ip, argv[1], INET6_ADDRSTRLEN);
-    info->port = atoi(argv[2]);
-}
-
-/**
- * Todo:
- * - indicator socket
- * - select vs poll usage
- * - chat protocol
- */
 int main(int argc, char **argv)
 {
     if (argc != 3) {
-        fprintf(stderr, "usage: %s <ip> <port>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <ip> <port>\n", argv[0]);
         return 1;
     } else if (strlen(argv[1]) > INET6_ADDRSTRLEN) {
-        fprintf(stderr, "usage: <ip> less then  %d bytes\n", INET6_ADDRSTRLEN);
+        fprintf(stderr, "Usage: <ip> less then  %d bytes\n", INET6_ADDRSTRLEN);
         return 1;
     }
 
-    tcpinfo_t info;
-    tcpinfo_init(&info, argc, argv);
+    struct llist_t queue;
+    llist_init(&queue);
+    
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return 1;
+    }
 
-    printf("Opening connection\n");
-    tcpcon_t conn;
-    if (tcpcon_init(&conn, &info))
+    printf("Opening connection\n"); 
+    elpinfo_t einfo;
+    einfo.pd = pipefd[0];
+    einfo.queue = &queue;
+    if (tcpcon_init(&einfo.con, argv[1], atoi(argv[2])))
         return 1;
 
     pthread_t eloop;
-    pthread_create(&eloop, NULL, run_eloop, &conn);
+    pthread_create(&eloop, NULL, run_eloop, &einfo);
 
-    char input[512];
+    char input[512], *msg;
+    int len;
     while (1) {
         if (!fgets(input, 512, stdin)) {
             if (ferror(stdin)) {
@@ -167,11 +188,15 @@ int main(int argc, char **argv)
             break;
         }
 
-        printf("stdin: %s", input);
+        len = strlen(input) + 1;
+        msg = malloc(len);
+        strncpy(msg, input, len);
+        llist_addl(&queue, msg);
+        write(pipefd[1], "1", 1);
     }
 
     printf("Closing connection\n");
     stop_thread(eloop);
-    tcpcon_destroy(&conn);
+    tcpcon_destroy(&einfo.con);
     return 0;
 }
